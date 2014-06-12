ffda7a4527d0190466411004bb2bc42d69ac2a68
Michael Huelsman <michael.huelsman@gmail.com>
2014-06-12 10:46:15 -0400

    Basic genetic operators

---
+++ Genetics/Genetics.cpp
@@ -0,0 +1,52 @@
+/*FILE: Genetics.cpp
+ *Author: Michael Huelsman
+ *Abstract:
+ *   Implementation of algortihms in Genetics.hpp 
+ */
+
+//needed subfuntions
+void randomize(Object gen[], int cycles);
+
+Children crossover(Parents p){
+  int crossed = 0;
+  bool father = true;
+  voxel first[NUM_VOX];
+  voxel second[NUM_VOX];
+  for(int i = 0;i < NUM_VOX;i++){
+    if(rand()%100 < CROSS_PER && crossed < CROSS_POINTS){
+      father = !father;
+      crossed++;
+    }
+    first[i] = father?p.father.getVoxels[i]:p.mother.getVoxels[i];
+    second[i] = father?p.mother.getVoxels[i]:p.father.getVoxels[i];
+  }
+  mutate(first,NUM_VOX);
+  mutate(second,NUM_VOX);
+  Children temp;
+  temp.first = Object(first);
+  temp.second = Object(second);
+}
+
+void mutate(voxel v[], int size){
+  for(int i = 0;i < size;i++){
+    if(rand()%100 < MUTATION_PER){
+      int *mutation = new int;
+      *mutation = rand();
+      v[i].x = ((char *)mutation)[1];
+      v[i].y = ((char *)mutation)[2];
+      v[i].z = ((char *)mutation)[3];
+    }
+  }
+}
+
+Parents selection(Object gen[], int size){
+  randomize(gen,size*2);
+  genSort(gen,SELECTION_SIZE);
+  Parents result;
+  result.father = gen[0];
+  result.mother = gen[1];
+  return result;
+}
+
+void genSort(Object *gen, int size){
+}
---
+++ Genetics/Genetics.hpp
@@ -0,0 +1,29 @@
+/*FILE: Genetics.hpp
+ *Author: Michael Huelsman
+ *Abstract:
+ *   Genetic operators of selection, crossover, and mutation
+ */
+
+#include "Object.hpp"
+
+const int CROSS_PER = 20;
+const int CROSS_POINTS = 1;
+const int MUTATION_PER = 1;
+const int SELECTION_SIZE = 10;
+
+
+struct Children{
+  Object first;
+  Object second;
+};
+
+struct Parents{
+  Object father;
+  Object mother;
+};
+
+
+Children crossover(Parents p);
+void mutate(voxel v[], int size);
+Parents selection(Object gen[], int size);
+void genSort(Object *gen, int size);
