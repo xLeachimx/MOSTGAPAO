9000863a005468949012b74a2c67342ce48b5d65
Michael Huelsman <michael.huelsman@gmail.com>
2014-06-23 10:18:36 -0400

    Automated doing multiple runs

--- main.cpp
+++ main.cpp
@@ -23,17 +23,12 @@ using std::time;
 const int NUM_GEN = 1000;
 const int GEN_SIZE = 100;
 const int ELITISM = 2;
+const int RUNS = 5;
 
 void primSoup(Object generation[], int size);//generates a random first generation
 
 int main(int argc, char **argv){
   if(argc != 3)return 0; //Make sure that there are the required number of commandline arguments
-
-  //setup
-  unsigned int seed = time(NULL);
-  srand(seed);
-  Object generation[GEN_SIZE];
-  Object nextGen[GEN_SIZE];
 
   //setup file for record keeping
   ofstream fout;
@@ -43,23 +38,62 @@ int main(int argc, char **argv){
     return 0;
   }
 
-  //record basic info
-  fout << "Number of generations:" << NUM_GEN << endl;
-  fout << "Generation Size:" << GEN_SIZE << endl;
-  fout << "Elitism:" << ELITISM << endl;
-  fout << "Number of Voxels:" << NUM_VOX << endl;
-  fout << "Seed:" << seed << endl;
-
-  //primordial soup generation
-  primSoup(generation, GEN_SIZE);
-
-  fout << "Generation,Connectivity,Phi Rating,Complexity,Symmetry" <<endl;//simple explanatory labels
-
-  //running the GA
-  for(int i = 0;i < NUM_GEN;i++){
-    cout << i <<endl;
-
-    //calcuate the fitness of all objects
+  for(int run = 0; run < RUNS; run++){
+    cout << "Run:" << run <<endl;
+    //setup
+    unsigned int seed = time(NULL);
+    srand(seed);
+    Object generation[GEN_SIZE];
+    Object nextGen[GEN_SIZE];
+    
+    //record basic info
+    fout << "Number of generations:" << NUM_GEN << endl;
+    fout << "Generation Size:" << GEN_SIZE << endl;
+    fout << "Elitism:" << ELITISM << endl;
+    fout << "Number of Voxels:" << NUM_VOX << endl;
+    fout << "Seed:" << seed << endl;
+    
+    //primordial soup generation
+    primSoup(generation, GEN_SIZE);
+    
+    fout << "Generation,Connectivity,Phi Rating,Complexity,Symmetry" <<endl;//simple explanatory labels
+    
+    //running the GA
+    for(int i = 0;i < NUM_GEN;i++){
+      
+      //calcuate the fitness of all objects
+      for(int j = 0;j < GEN_SIZE;j++){
+	generation[j].calcQuality();
+      }
+      for(int j = 0;j < GEN_SIZE;j++){
+	generation[j].calcFitness(generation, GEN_SIZE);
+      }
+      genSort(generation,GEN_SIZE);//sort according to dominance
+      
+      //record keeping
+      fout << i << ",";
+      generation[0].toCSV(fout);
+      
+      //elitism
+      for(int j = 0;j < ELITISM;j++){
+	nextGen[j] = generation[j];
+      }
+      
+      //reproduction
+      Children product;
+      for(int j = 0;j < ((GEN_SIZE-ELITISM)/2);j++){
+	product = crossover(selection(generation,GEN_SIZE));
+	nextGen[ELITISM+(2*j)] = product.first;
+	nextGen[ELITISM+(2*j)+1] = product.second;
+      }
+      
+      //copying generation over to the next
+      for(int j = 0;j < GEN_SIZE;j++){
+	generation[j] = nextGen[j];
+      }
+    }
+    
+    //post-run records
     for(int j = 0;j < GEN_SIZE;j++){
       generation[j].calcQuality();
     }
@@ -69,48 +103,20 @@ int main(int argc, char **argv){
     genSort(generation,GEN_SIZE);//sort according to dominance
     
     //record keeping
-    fout << i << ",";
+    fout << NUM_GEN << ",";
     generation[0].toCSV(fout);
-    
-    //elitism
-    for(int j = 0;j < ELITISM;j++){
-      nextGen[j] = generation[j];
-    }
-    
-    //reproduction
-    Children product;
-    for(int j = 0;j < ((GEN_SIZE-ELITISM)/2);j++){
-      product = crossover(selection(generation,GEN_SIZE));
-      nextGen[ELITISM+(2*j)] = product.first;
-      nextGen[ELITISM+(2*j)+1] = product.second;
-    }
-    
-    //copying generation over to the next
-    for(int j = 0;j < GEN_SIZE;j++){
-      generation[j] = nextGen[j];
-    }
-  }
-  
-  //post-run records
-  for(int j = 0;j < GEN_SIZE;j++){
-    generation[j].calcQuality();
-  }
-  for(int j = 0;j < GEN_SIZE;j++){
-    generation[j].calcFitness(generation, GEN_SIZE);
-  }
-  genSort(generation,GEN_SIZE);//sort according to dominance
-  
-  //record keeping
-  fout << NUM_GEN << ",";
-  generation[0].toCSV(fout);
-  fout.close();
 
-  //export best of best to scad file
-  fout.open(argv[2]);
-  if(!fout){
-    cout << "Problem opening scad file" <<endl;
+    if(run == RUNS-1){
+      //export best of best to scad file
+      ofstream out;
+      out.open(argv[2]);
+      if(!out){
+	cout << "Problem opening scad file" <<endl;
+      }
+      generation[0].toScad(out);
+      out.close();
+    }
   }
-  generation[0].toScad(fout);
   fout.close();
   return 0;
 }
